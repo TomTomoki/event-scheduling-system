3df77f811a743002db1c1ecb16177ad67b982387
Tomoki Kyotani <tkyotani121@gmail.com>
2023-03-22 17:27:41 -0700

    Change primary key data type to serial

--- Queries/DDL.sql
+++ Queries/DDL.sql
@@ -1,12 +1,12 @@
 create table Faculty (
-	FacultyID int primary key,
+	FacultyID serial primary key,
 	FacultyFirstName VARCHAR(20) not null,
 	FacultyLastName VARCHAR(20) not null,
 	FacultyEmail VARCHAR(50) not null
 );
 
 create table Course(
-	CourseID int primary key,
+	CourseID serial primary key,
 	CourseName VARCHAR(50) not null,
 	InstructorID int references Faculty (FacultyID) ON DELETE SET NULL ON UPDATE CASCADE,
 	AcademicYear numeric(4) not null,
@@ -14,25 +14,25 @@ create table Course(
 );
 
 CREATE TABLE Building(
-    BuildingID int primary key,
+    BuildingID serial primary key,
     BuildingName VARCHAR(10) NOT NULL,
     BuildingAddress VARCHAR(50) NOT NUll
 );
 
 create table Room (
-	RoomID int primary key,
+	RoomID serial primary key,
 	RoomName VARCHAR(15) not null,
 	BuildingID int references Building (BuildingID) ON DELETE CASCADE ON UPDATE CASCADE,
 	Capacity int not null
 );
 
 create table Department (
-	DepartmentID int primary key,
+	DepartmentID serial primary key,
 	DepartmentName VARCHAR(20)
 );
 
 create table Staff (
-	StaffID int primary key,
+	StaffID serial primary key,
 	DepartmentID int references Department (DepartmentID) ON DELETE SET NULL ON UPDATE CASCADE,
 	StaffFirstName VARCHAR(20) not null,
 	StaffLastName VARCHAR(20) not null,
@@ -40,14 +40,14 @@ create table Staff (
 );
 
 create table Event (
-	EventID int primary key,
+	EventID serial primary key,
 	EventName VARCHAR(50) not null,
 	MainContact int references Staff (StaffID) ON DELETE SET NULL ON UPDATE CASCADE,
 	SecondContact int references Staff (StaffID) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 create table RoomAssignment (
-	RoomAssignmentID int primary key,
+	RoomAssignmentID serial primary key,
 	EventID int references Event (EventID) ON DELETE CASCADE ON UPDATE CASCADE,
 	CourseID int references Course (CourseID) ON DELETE CASCADE ON UPDATE CASCADE,
 	RoomID int not null references Room (RoomID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -56,7 +56,7 @@ create table RoomAssignment (
 );
 
 create table Schedule (
-	ScheduleID int primary key,
+	ScheduleID serial primary key,
 	RoomAssignmentID int references RoomAssignment (RoomAssignmentID) ON DELETE SET NULL ON UPDATE CASCADE,
     ScheduledDate date,
 	StartTime time with time zone not null,
--- Queries/SampleData.sql
+++ Queries/SampleData.sql
@@ -1,61 +1,61 @@
-insert into Faculty (FacultyID, FacultyFirstName, FacultyLastName, FacultyEmail) values 
-(1, 'Tom', 'Peter', 'tom.p@example.com'),
-(2, 'John', 'Smith', 'john.s@example.com'),
-(3, 'Mike', 'Yamada', 'mike.y@example.com'),
-(4, 'Kaz', 'Johnson', 'kaz.j@example.com'),
-(5, 'Luis', 'Peter', 'luis.p@example.com');
+insert into Faculty (FacultyFirstName, FacultyLastName, FacultyEmail) values 
+('Tom', 'Peter', 'tom.p@example.com'),
+('John', 'Smith', 'john.s@example.com'),
+('Mike', 'Yamada', 'mike.y@example.com'),
+('Kaz', 'Johnson', 'kaz.j@example.com'),
+('Luis', 'Peter', 'luis.p@example.com');
 
 
-insert into Course (CourseID, CourseName, InstructorID, AcademicYear, AcademicSemester) values
-(1, 'DAMG7275', 1, 2023, 'Fall'),
-(2, 'IE6800', 3, 2023, 'Fall'),
-(3, 'CS5800', 5, 2023, 'Fall');
+insert into Course (CourseName, InstructorID, AcademicYear, AcademicSemester) values
+('DAMG7275', 1, 2023, 'Fall'),
+('IE6800', 3, 2023, 'Fall'),
+('CS5800', 5, 2023, 'Fall');
 
 
-insert into Building (BuildingID, BuildingName, BuildingAddress) values
-(1, '401', '401 Terry Ave N #103, Seattle, WA 98109'),
-(2, '225', '225 Terry Ave N, Seattle, WA 98109');
+insert into Building (BuildingName, BuildingAddress) values
+('401', '401 Terry Ave N #103, Seattle, WA 98109'),
+('225', '225 Terry Ave N, Seattle, WA 98109');
 
 
-insert into Room (RoomID, RoomName, BuildingID, Capacity) values
-(1, '202', 2, 50),
-(2, '301', 2, 50),
-(3, '102', 1, 100);
+insert into Room (RoomName, BuildingID, Capacity) values
+('202', 2, 50),
+('301', 2, 50),
+('102', 1, 100);
 
 
-insert into Department (DepartmentID, DepartmentName) values
-(1, 'IT Services'),
-(2, 'Campus Operations'),
-(3, 'Career Development');
+insert into Department (DepartmentName) values
+('IT Services'),
+('Campus Operations'),
+('Career Development');
 
 
-insert into Staff (StaffID, DepartmentID, StaffFirstName, StaffLastName, StaffEmail) values
-(1, 1, 'Thomas', 'John', 'thomas@example.com'),
-(2, 2, 'Aya', 'Ishida', 'aya@example.com'),
-(3, 3, 'Bill', 'Jassy', 'bill@example.com');
+insert into Staff (DepartmentID, StaffFirstName, StaffLastName, StaffEmail) values
+(1, 'Thomas', 'John', 'thomas@example.com'),
+(2, 'Aya', 'Ishida', 'aya@example.com'),
+(3, 'Bill', 'Jassy', 'bill@example.com');
 
 
-insert into Event (EventID, EventName, MainContact, SecondContact) values
-(1, 'Career Fair', 3, 2),
-(2, 'Alumni Networking', 3, 2),
-(3, 'IT Support Weekly Session', 1, 2);
+insert into Event (EventName, MainContact, SecondContact) values
+('Career Fair', 3, 2),
+('Alumni Networking', 3, 2),
+('IT Support Weekly Session', 1, 2);
 
 
 -- Event
-insert into RoomAssignment (RoomAssignmentID, EventID, RoomID, ExpectedParticipant, Detail) values
-(1, 1, 3, 95, '2023 Spring STEM Career Fair'),
-(2, 2, 1, 40, '2023 Fall CPS Alumni Gathering');
+insert into RoomAssignment (EventID, RoomID, ExpectedParticipant, Detail) values
+(1, 3, 95, '2023 Spring STEM Career Fair'),
+(2, 1, 40, '2023 Fall CPS Alumni Gathering');
 
 -- Course
-insert into RoomAssignment (RoomAssignmentID, CourseID, RoomID, ExpectedParticipant, Detail) values
-(3, 1, 2, 40, 'Seattle campus - session A'), 
-(4, 2, 2, 30, 'Seattle campus - session A'), 
-(5, 3, 1, 25, 'Seattle campus - session C');
+insert into RoomAssignment (CourseID, RoomID, ExpectedParticipant, Detail) values
+(1, 2, 40, 'Seattle campus - session A'), 
+(2, 2, 30, 'Seattle campus - session A'), 
+(3, 1, 25, 'Seattle campus - session C');
 
 
-insert into Schedule (ScheduleID, RoomAssignmentID, ScheduledDate, StartTime, EndTime, Recurring, DayOfWeek, RecurringStartDate, RecurringEndDate) values
-(1, 1, '2023-05-05', '10:00:00 PST', '17:00:00 PST', FALSE, null, null, null),
-(2, 2, '2023-04-21', '18:00:00 PST', '20:00:00 PST', FALSE, null, null, null),
-(3, 3, null, '13:00:00 PST', '15:00:00 PST', TRUE, 'Wed', '2023-09-10', '2023-12-15'),
-(4, 4, null, '10:00:00 PST', '12:00:00 PST', TRUE, 'Mon', '2023-09-10', '2023-12-15'),
-(5, 4, null, '10:00:00 PST', '12:00:00 PST', TRUE, 'Thu', '2023-09-10', '2023-12-15');
+insert into Schedule (RoomAssignmentID, ScheduledDate, StartTime, EndTime, Recurring, DayOfWeek, RecurringStartDate, RecurringEndDate) values
+(1, '2023-05-05', '10:00:00 PST', '17:00:00 PST', FALSE, null, null, null),
+(2, '2023-04-21', '18:00:00 PST', '20:00:00 PST', FALSE, null, null, null),
+(3, null, '13:00:00 PST', '15:00:00 PST', TRUE, 'Wed', '2023-09-10', '2023-12-15'),
+(4, null, '10:00:00 PST', '12:00:00 PST', TRUE, 'Mon', '2023-09-10', '2023-12-15'),
+(4, null, '10:00:00 PST', '12:00:00 PST', TRUE, 'Thu', '2023-09-10', '2023-12-15');
